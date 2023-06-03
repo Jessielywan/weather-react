ffbd5f1edd3edaae9c836c4130e6cf38652c0b3e
Jess <jessielywan@outlook.com>
2023-06-03 10:26:01 +0200

    Added a working search function

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import CurrentDate from "./CurrentDate";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ready: false});
   function handleResponse(response) {
     setWeatherData({
@@ -19,15 +19,37 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "o3d77t41a2a0ce9d42f448b54f6283dd";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
           <div className="container">
             <div className="search-form">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-9">
-                    <input type="search" placeholder="Enter a city.." className="form-control search-input" autoComplete="off" />
+                    <input 
+                      type="search" 
+                      placeholder="Enter a city.."
+                      className="form-control search-input" 
+                      autoComplete="off"
+                      onChange={handleCityChange} 
+                    />
                   </div>
                   <div className="col-3 p-0">
                     <input type="submit" className="btn btn-primary w-100" value="Search" />
@@ -96,16 +118,9 @@ export default function Weather(props) {
       </div>
   );
   } else {
-    const apiKey = "o3d77t41a2a0ce9d42f448b54f6283dd";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 
 
-
-
-  
-    
 }
