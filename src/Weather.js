3dab8c49c50b940345f166d57713822055f47779
Jess <jessielywan@outlook.com>
2023-06-04 22:03:08 +0200

    Deleted the weather forecast for now

--- src/Weather.js
+++ src/Weather.js
@@ -59,58 +59,6 @@ export default function Weather(props) {
               </form>
               <WeatherInfo data={weatherData} />
             </div>
-            <div className="weather-forecast">
-              <div className="row">
-                <div className="col-2">
-                  <div className="weather-forecast-date">Thu</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">20°</span>
-                    <span className="weather-forecast-temperature-min">12°</span>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <div className="weather-forecast-date">Fri</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">15°</span>
-                    <span className="weather-forecast-temperature-min">9°</span>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <div className="weather-forecast-date">Sat</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">23°</span>
-                    <span className="weather-forecast-temperature-min">14°</span>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <div className="weather-forecast-date">Sun</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">22°</span>
-                    <span className="weather-forecast-temperature-min">16°</span>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <div className="weather-forecast-date">Mon</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">17°</span>
-                    <span className="weather-forecast-temperature-min">10°</span>
-                  </div>
-                </div>
-                <div className="col-2">
-                  <div className="weather-forecast-date">Mon</div>
-                  <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png" alt="" width="90"></img>
-                  <div className="weather-forecast-temperatures">
-                    <span className="weather-forecast-temperature-max">17°</span>
-                    <span className="weather-forecast-temperature-min">10°</span>
-                  </div>
-                </div>
-              </div>
-            </div>
             <footer>
               <a href="https://github.com/Jessielywan/weather-react" target="_blank" rel="noopener noreferrer">Open-source Code</a> by Jessie Wan
             </footer>
