79c92c829b1e75978b0435ab2245ee2cebadff6b
Jess <jessielywan@outlook.com>
2023-06-10 15:35:08 +0200

    Fixed the Forecast row

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -12,28 +12,28 @@ export default function WeatherForecast(props) {
     }, [props.coordinates]);
 
     function handleResponse(response) {
+      console.log(response.data);
         setForecast(response.data.daily);
-        
         setLoaded(true);
     }
 
     if (loaded) {
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                {forecast.map(function (dailyForecast, index) {
-                  if (index < 5) {
-                    return (
-                      <div className="col" key={index}>
-                      <WeatherForecastDay data={dailyForecast} />
-                    </div>
-                    );
-                  }
-                })}
-            </div>
+        return (
+    <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index ) {
+            if (index < 5) {
+            return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            };
+            })}
+            
         </div>
+    </div>
     );
-
     }else {        
         let apiKey = "o3d77t41a2a0ce9d42f448b54f6283dd";
         let latitude = props.coordinates.latitude;
