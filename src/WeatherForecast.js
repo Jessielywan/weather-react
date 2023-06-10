dff51c5dbe2941edf6584848163820d522288781
Jess <jessielywan@outlook.com>
2023-06-10 10:42:05 +0200

    Added the Forecast row

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,21 +8,28 @@ export default function WeatherForecast(props) {
     let [forecast, setForecast] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data);
-        setForecast(response.data);
+        setForecast(response.data.daily);
+        
         setLoaded(true);
     }
 
     if (loaded) {
-        return (
-    <div className="WeatherForecast">
-        <div className="row">
-            <div className="col">
-                <WeatherForecastDay data={forecast} />
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                {forecast.map(function (dailyForecast, index) {
+                  if (index < 5) {
+                    return (
+                      <div className="col" key={index}>
+                      <WeatherForecastDay data={dailyForecast} />
+                    </div>
+                    );
+                  }
+                })}
             </div>
         </div>
-    </div>
     );
+
     }else {        
         let apiKey = "o3d77t41a2a0ce9d42f448b54f6283dd";
         let latitude = props.coordinates.latitude;
